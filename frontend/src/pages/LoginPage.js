65b5155917198641e39c43a19fe654d48cf653d5
AdaamEL <91120983+AdaamEL@users.noreply.github.com>
2026-02-06 16:47:57 +0100

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -122,15 +122,6 @@ export default function LoginPage() {
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300" />
               </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-white dark:bg-slate-800/80 text-slate-500">
-                  Ou continuer avec
-                </span>
-              </div>
-            </div>
-
-            <div className="mt-6">
-              <GoogleSignInButton />
             </div>
           </div>
 
